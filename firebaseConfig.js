862ae21a258e4af8ac0f4ebea68994d9c9e09451
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2024-11-02 00:58:06 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
+import * as firebase from 'firebase/app';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
