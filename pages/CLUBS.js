252a527990e5494f9cb5da43c62fd59978b405c1
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2025-05-09 22:23:23 +0200

    Update CLUBS.js

--- pages/CLUBS.js
+++ pages/CLUBS.js
@@ -13,7 +13,7 @@ const CLUBS = () => {
       {/* Sections */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
 
-        {/* Clubs At a Glance Section */}
+        {/* Clubs At Glance Section */}
         <div className="relative bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="relative h-48 w-full">
             <Image
