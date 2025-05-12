0053d328bc989f4bacbac5ba190c038d25d6cd68
Masimbakutendaishe <49595768+Masimbakutendaishe@users.noreply.github.com>
2025-05-12 20:43:08 +0200

    Update ADMISSIONS.js

--- pages/ADMISSIONS.js
+++ pages/ADMISSIONS.js
@@ -10,7 +10,7 @@ const ADMISSIONS = () => {
         <h1 className="relative text-left text-white text-5xl font-bold z-10 py-16">ADMISSIONS</h1>
       </div>
 
-      {/* Sections */}
+      {/* The Sections */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
         {/* Application Process and Forms Section */}
         <div className="relative bg-white rounded-lg shadow-lg overflow-hidden">
